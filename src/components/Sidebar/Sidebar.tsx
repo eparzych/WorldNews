bd30d1ae7c20f9f7cedbba47159b25a95a14822e
eparzych <emilia.parzych92@gmail.com>
2023-03-23 19:40:45 +0100

    Added Sidebar with country code and flag

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import Flag from "react-world-flags";
+import { countryList } from "../../app/countries";
 import "./sidebar.scss";
 
 export const Sidebar = () => {
     return(
         <div className="sidebar">
-            <p>Sidebar</p>
+            <h3 className="country__title">INFORMACJE Z KRAJU</h3>
+            <ul className="country__list">
+                { countryList.map(country =>
+                    <li key={country.code} className="country__item">
+                        <a href="" target="_blank"><Flag code={country.code} height="18" /> {country.name}</a>
+                    </li>
+                )}
+            </ul>
         </div>
     )
 }
