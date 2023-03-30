5ac7552922db27445bbcd7d9e173506f9dad9628
eparzych <emilia.parzych92@gmail.com>
2023-03-30 19:17:16 +0200

    Added sidebar closing after country selection

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,21 +1,24 @@
-import { Link, BrowserRouter as Router, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Flag from "react-world-flags";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import type { RootState } from "../../app/store";
+import { closeSidebar } from "../../features/sidebar/sidebarSlice";
 import countryList from "../../app/countries.json";
 import "./sidebar.scss";
 
 export const Sidebar = () => {
     const sidebarOpen = useSelector((state: RootState) => state.sidebar.isOpen);
     const {countryCode} = useParams();
+    const dispatch = useDispatch();
 
     return(
         <div className={`sidebar ${sidebarOpen ? "sidebar--open" : ""}`}>
             <h3 data-testid="sidebar-title" className="country__title">INFORMACJE Z KRAJU</h3>
                 <ul className="country__list">
                     { countryList.map((country) =>
-                        <li key={country.code} >
-                            <Link to={`/country/${country.code}`} 
+                        <li key={country.code}>
+                            <Link to={`/country/${country.code}`}
+                                onClick={() => dispatch(closeSidebar())} 
                                 className={`country__item ${countryCode === country.code ? 'country--active' : ''}`}
                             >
                                 <Flag code={country.code} 
